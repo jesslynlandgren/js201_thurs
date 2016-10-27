95db683efe260dd6250ffdca4985fa1e5ebf38e7
Jesslyn Landgren <jesslyn.landgren@gmail.com>
2016-10-27 09:32:45 -0400

    finished object dictionary warmup exercises

--- obj_dictionaries.js
+++ obj_dictionaries.js
@@ -11,3 +11,27 @@ phonebookDict.Bob = '968-345-2345';
 for (var entry in phonebookDict) {
     console.log(entry + ':  ' + phonebookDict[entry]);
 }
+
+function histogram(string) {
+    tally = {};
+    for (i=0; i<string.length; i++) {
+        if (!(string[i] in tally)) {
+            tally[string[i]] = 1;
+        } else {
+            tally[string[i]] += 1;
+        }
+    }
+    return tally;
+}
+console.log(histogram('bananas'));
+
+function topTwo(string) {
+    tally = histogram(string);
+    var arrayTally = [];
+    for (var letter in tally) {
+        arrayTally.push([letter, tally[letter]])
+    }
+    arrayTally.sort(function (a,b) {return b[1] - a[1];});
+    return [arrayTally[0][0], arrayTally[1][0]];
+}
+console.log(topTwo('bananas'));
